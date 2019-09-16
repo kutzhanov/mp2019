a238213af5ea899e46ab607eabbab753bcd5a4d6
Timur Kutzhanov <kutzhanov@gmail.com>
2019-09-16 19:23:04 +0600

    few changes

--- 04_docker/02_web/index.php
+++ 04_docker/02_web/index.php
@@ -1,15 +1,16 @@
 <?php
-echo gethostname();
-
 $servername = getenv('MYSQL_SERVERNAME');
 $username = file_get_contents(getenv('MYSQL_USER_FILE'));
 $password = file_get_contents(getenv('MYSQL_PASSWORD_FILE'));
 $hostname = gethostname();
+
+echo  "Host name: " + $hostname + "\r\n"
+
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     die("$hostname connection failed: " . $conn->connect_error);
 }
-echo "$hostname successfully connected to $servername";
+echo "Host $hostname successfully connected to $servername";
 ?>
